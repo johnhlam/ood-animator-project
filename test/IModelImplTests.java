e855d7f00288611feb672acc402e48c316009076
John L <lam.joh@husky.neu.edu>
2019-06-06 16:30:05 -0400

    JFKDlfa

--- test/IModelImplTests.java
+++ test/IModelImplTests.java
@@ -22,6 +22,37 @@ public class IModelImplTests {
     model1 = new IModelImpl();
   }
 
+  /**
+   * Tests IModelImpl#printAnimations() with a model that has no IShapes in it.
+   */
+  @Test
+  public void testPrintEmpty() {
+    assertEquals("", model1.printAnimations());
+  }
+
+  /**
+   * Tests IModelImpl#printAnimations() with a model that has 1 (rectangle) IShape in it, without
+   * any motions in the shape.
+   */
+  @Test
+  public void testPrintEmptyRect() {
+    model1.addShape("R", ShapeType.RECTANGLE, 10, 20, Color.CYAN, 30, 40);
+
+    assertEquals("shape R rectangle", model1.printAnimations());
+  }
+
+  /**
+   * Tests IModelImpl#printAnimations() with a model that has 1 (ellipse) IShape in it, without
+   * any motions in the shape.
+   */
+  @Test
+  public void testPrintEmptyEll() {
+    model1.addShape("C", ShapeType.ELLIPSE, 10, 20, Color.CYAN, 30, 40);
+
+    assertEquals("shape C ellipse", model1.printAnimations());
+  }
+
+
   @Test
   public void test() {
     model1.addShape("1", ShapeType.RECTANGLE, 10, 20, Color.BLACK, 0, 0);
